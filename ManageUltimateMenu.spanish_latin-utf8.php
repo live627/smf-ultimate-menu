542d75d7c576e9f8e5f9ea9342a13bbf1ee9c675
John Rayes <live627@gmail.com>
2016-04-07 18:26:35 -0700

    Added files via upload

---
+++ ManageUltimateMenu.spanish_latin-utf8.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package Ultimate Menu mod
+ * @version 1.0
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+ /**
+ * Latin Spanish translation services by RockLee_BC http://www.bombercode.org/
+ */
+
+$txt['um_admin_menu'] = 'Menú avanzado';
+$txt['um_admin_add_button'] = 'Añadir botón';
+$txt['um_admin_manage_menu'] = 'Administrar menú';
+
+$txt['admin_menu'] = 'Menú';
+$txt['admin_menu_title'] = 'Ajustes de menú';
+$txt['admin_menu_desc'] = 'Esta página le permite editar y añadir botones de menú especiales.';
+
+$txt['admin_manage_menu_submit'] = 'Enviar';
+$txt['admin_manage_menu_desc'] = 'Administrar el botón Menú que ha creado';
+$txt['admin_menu_add_button_desc'] = 'Añadir nuevos botones de menú';
+$txt['external_link'] = 'Enlace externo';
+$txt['forum_link'] = 'Enlace interno';
+$txt['active'] = 'Activo';
+$txt['inactive'] = 'Inactivo';
+$txt['um_menu_no_buttons'] = 'No hay botones sin embargo...';
+$txt['um_menu_button_id'] = 'Identificación de botón';
+$txt['um_menu_button_name'] = 'Nombre del botón';
+$txt['um_menu_button_type'] = 'Tipo de botón';
+$txt['um_menu_button_position'] = 'Posición del botón';
+$txt['um_menu_button_link'] = 'Enlace del botón';
+$txt['um_menu_actions'] = 'Comportamiento';
+$txt['modify'] = 'Editar';
+$txt['before'] = 'Antes de';
+$txt['unchanged'] = 'Sin alterar';
+$txt['after'] = 'Después';
+$txt['um_menu_remove_selected'] = 'Quitar botones seleccionados';
+$txt['um_menu_remove_all'] = 'Retirar todos los botones';
+$txt['um_menu_remove_confirm'] = '¿Seguro que desea eliminar los botones seleccionados?';
+$txt['um_menu_remove_all_confirm'] = '¿Está seguro de que desea eliminar todos los botones?';
+$txt['um_menu_add_title'] = 'Añadir botón';
+$txt['um_menu_edit_title'] = 'Editar Botón';
+$txt['um_menu_button_name'] = 'Nombre del botón';
+$txt['um_menu_button_type'] = 'Tipo de botón';
+$txt['um_menu_external'] = 'Enlace externo';
+$txt['um_menu_forum'] = 'Enlace interno';
+$txt['um_menu_button_link_desc'] = 'Para enlace del foro sólo se puede poner las cosas después de "index.php?" en el link de su foro.';
+$txt['um_menu_button_link'] = 'Enlace del botón';
+$txt['um_menu_button_perms'] = 'Grupos permitidos';
+$txt['um_menu_button_guest'] = 'Visitantes';
+$txt['um_menu_button_position'] = 'Posición del botón';
+$txt['um_menu_button_status'] = 'Estado del botón';
+$txt['um_menu_button_active'] = 'Activo';
+$txt['um_menu_button_inactive'] = 'No activo';
+$txt['um_menu_link_type'] = 'Tipo de enlace';
+$txt['um_menu_same_window'] = 'En la misma ventana';
+$txt['um_menu_new_tab'] = 'Nueva pestaña';
+
+
+// Submission errors
+$txt['um_menu_not_found'] = 'El botón que trató de editar no existe!';
+$txt['um_menu_errors_create'] = 'Se ha producido el siguiente error o errores al tiempo de añadir su botón:';
+$txt['um_menu_errors_modify'] = 'Se ha producido el siguiente error o errores durante la edición de su botón:';
+$txt['um_menu_numeric_desc'] = 'El nombre del botón que eligió es todo numérico. Debe utilizar un nombre que contenga al menos un carácter numérico. <br />
+1E5 se considera numérico (notación científica) 1,5 se considera numérico (número decimal)';
+$txt['um_menu_empty_name'] = 'El nombre se dejo vacío.';
+$txt['um_menu_empty_link'] = 'El enlace se dejo vacío.';
+$txt['um_menu_empty_parent'] = 'La matriz se dejo vacío.';
+$txt['um_menu_depend_desc'] = 'Debe eliminar todos los botones de la parte inferior del botón antes de mover o alterarlos.';
+$txt['um_menu_before_child_title'] = 'Haga este botón inferior';
+$txt['um_menu_before_child_desc'] = 'Utilice el botón el botón inferior en el botón inferior para hacer otra!';
+$txt['um_menu_mysql'] = 'El nombre del botón que eligió ya está en uso!';
+
+?>
---
+++ ManageUltimateMenu.spanish_latin.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package Ultimate Menu mod
+ * @version 1.0
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+ /**
+ * Latin Spanish translation services by RockLee_BC http://www.bombercode.org/
+ */
+
+$txt['um_admin_menu'] = 'Men&uacute; avanzado';
+$txt['um_admin_add_button'] = 'A&ntilde;adir bot&oacute;n';
+$txt['um_admin_manage_menu'] = 'Administrar men&uacute;';
+
+$txt['admin_menu'] = 'Men&uacute;';
+$txt['admin_menu_title'] = 'Ajustes de men&uacute;';
+$txt['admin_menu_desc'] = 'Esta p&aacute;gina le permite editar y a&ntilde;adir botones de men&uacute; especiales.';
+
+$txt['admin_manage_menu_submit'] = 'Enviar';
+$txt['admin_manage_menu_desc'] = 'Administrar el bot&oacute;n Men&uacute; que ha creado';
+$txt['admin_menu_add_button_desc'] = 'A&ntilde;adir nuevos botones de men&uacute;';
+$txt['external_link'] = 'Enlace externo';
+$txt['forum_link'] = 'Enlace interno';
+$txt['active'] = 'Activo';
+$txt['inactive'] = 'Inactivo';
+$txt['um_menu_no_buttons'] = 'No hay botones sin embargo...';
+$txt['um_menu_button_id'] = 'Identificaci&oacute;n de bot&oacute;n';
+$txt['um_menu_button_name'] = 'Nombre del bot&oacute;n';
+$txt['um_menu_button_type'] = 'Tipo de bot&oacute;n';
+$txt['um_menu_button_position'] = 'Posici&oacute;n del bot&oacute;n';
+$txt['um_menu_button_link'] = 'Enlace del bot&oacute;n';
+$txt['um_menu_actions'] = 'Comportamiento';
+$txt['modify'] = 'Editar';
+$txt['before'] = 'Antes de';
+$txt['unchanged'] = 'Sin alterar';
+$txt['after'] = 'Despu&eacute;s';
+$txt['um_menu_remove_selected'] = 'Quitar botones seleccionados';
+$txt['um_menu_remove_all'] = 'Retirar todos los botones';
+$txt['um_menu_remove_confirm'] = '¿Seguro que desea eliminar los botones seleccionados?';
+$txt['um_menu_remove_all_confirm'] = '¿Est&aacute; seguro de que desea eliminar todos los botones?';
+$txt['um_menu_add_title'] = 'A&ntilde;adir bot&oacute;n';
+$txt['um_menu_edit_title'] = 'Editar Bot&oacute;n';
+$txt['um_menu_button_name'] = 'Nombre del bot&oacute;n';
+$txt['um_menu_button_type'] = 'Tipo de bot&oacute;n';
+$txt['um_menu_external'] = 'Enlace externo';
+$txt['um_menu_forum'] = 'Enlace interno';
+$txt['um_menu_button_link_desc'] = 'Para enlace del foro s&oacute;lo se puede poner las cosas despu&eacute;s de "index.php?" en el link de su foro.';
+$txt['um_menu_button_link'] = 'Enlace del bot&oacute;n';
+$txt['um_menu_button_perms'] = 'Grupos permitidos';
+$txt['um_menu_button_guest'] = 'Visitantes';
+$txt['um_menu_button_position'] = 'Posici&oacute;n del bot&oacute;n';
+$txt['um_menu_button_status'] = 'Estado del bot&oacute;n';
+$txt['um_menu_button_active'] = 'Activo';
+$txt['um_menu_button_inactive'] = 'No activo';
+$txt['um_menu_link_type'] = 'Tipo de enlace';
+$txt['um_menu_same_window'] = 'En la misma ventana';
+$txt['um_menu_new_tab'] = 'Nueva pesta&ntilde;a';
+
+
+// Submission errors
+$txt['um_menu_not_found'] = 'El bot&oacute;n que trat&oacute; de editar no existe!';
+$txt['um_menu_errors_create'] = 'Se ha producido el siguiente error o errores al tiempo de a&ntilde;adir su bot&oacute;n:';
+$txt['um_menu_errors_modify'] = 'Se ha producido el siguiente error o errores durante la edici&oacute;n de su bot&oacute;n:';
+$txt['um_menu_numeric_desc'] = 'El nombre del bot&oacute;n que eligi&oacute; es todo num&eacute;rico. Debe utilizar un nombre que contenga al menos un car&aacute;cter num&eacute;rico.<br />
+1E5 se considera num&eacute;rico (notaci&oacute;n cient&iacute;fica) 1,5 se considera num&eacute;rico (n&uacute;mero decimal)';
+$txt['um_menu_empty_name'] = 'El nombre se dejo vac&iacute;o.';
+$txt['um_menu_empty_link'] = 'El enlace se dejo vac&iacute;o.';
+$txt['um_menu_empty_parent'] = 'La matriz se dejo vac&iacute;o.';
+$txt['um_menu_depend_desc'] = 'Debe eliminar todos los botones de la parte inferior del bot&oacute;n antes de mover o alterarlos.';
+$txt['um_menu_before_child_title'] = 'Haga este bot&oacute;n inferior';
+$txt['um_menu_before_child_desc'] = 'Utilice el bot&oacute;n el bot&oacute;n inferior en el bot&oacute;n inferior para hacer otra!';
+$txt['um_menu_mysql'] = 'El nombre del bot&oacute;n que eligi&oacute; ya est&aacute; en uso!';
+
+?>
