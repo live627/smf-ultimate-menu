1166caf843a0ae275c54ee0dc0962999f0a65eca
realdigger <digger@mysmf.net>
2018-02-16 16:17:23 +0400

    Add Russian language.

---
+++ ManageUltimateMenu.russian-utf8.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @package Ultimate Menu mod
+ * @version 1.0.2
+ * @author John Rayes <live627@gmail.com>
+ * @translation digger <digger@mysmf.net>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+$txt['um_admin_menu'] = 'Управление меню';
+$txt['um_admin_add_button'] = 'Добавить пункт';
+$txt['um_admin_manage_menu'] = 'Управление меню';
+
+$txt['admin_menu'] = 'Меню';
+$txt['admin_menu_title'] = 'Редактирование пункта меню';
+$txt['admin_menu_desc'] = 'На этой странице вы можете добавлять и редактировать пункты меню.';
+
+$txt['admin_manage_menu_submit'] = 'Сохранить';
+$txt['admin_manage_menu_desc'] = 'Управление вашими пунктами меню';
+$txt['admin_menu_add_button_desc'] = 'Добавить новый пункт в меню';
+$txt['external_link'] = 'Внешняя ссылка';
+$txt['forum_link'] = 'Ссылка на страницу форума';
+$txt['active'] = 'Включен';
+$txt['inactive'] = 'Выключен';
+$txt['um_menu_no_buttons'] = 'Еще нет добавленных пунктов...';
+$txt['um_menu_button_id'] = 'ID';
+$txt['um_menu_button_name'] = 'Название';
+$txt['um_menu_button_type'] = 'Тип';
+$txt['um_menu_button_position'] = 'Позиция';
+$txt['um_menu_button_link'] = 'Ссылка';
+$txt['um_menu_actions'] = 'Действия';
+$txt['modify'] = 'Редактировать';
+$txt['before'] = 'Перед';
+$txt['unchanged'] = 'Не менять';
+$txt['after'] = 'После';
+$txt['um_menu_remove_selected'] = 'Удалить выделенные пункты';
+$txt['um_menu_remove_all'] = 'Удалить все пункты';
+$txt['um_menu_remove_confirm'] = 'Уверены, что хотите удалить выделенные пункты?';
+$txt['um_menu_remove_all_confirm'] = 'Уверены, что хотите удалить все пункты?';
+$txt['um_menu_add_title'] = 'Добавить';
+$txt['um_menu_edit_title'] = 'Отредактировать';
+$txt['um_menu_external'] = 'Внешняя ссылка';
+$txt['um_menu_forum'] = 'Ссылка на страницу форума';
+$txt['um_menu_button_link_desc'] = 'Для ссылок на страницы форума можно использовать только то, что идет после index.php?, например, action=unread';
+$txt['um_menu_button_perms'] = 'Права доступа';
+$txt['um_menu_button_guest'] = 'Гости';
+$txt['um_menu_button_status'] = 'Состояние';
+$txt['um_menu_button_active'] = 'Включен';
+$txt['um_menu_button_inactive'] = 'Выключен';
+$txt['um_menu_link_type'] = 'Открывать ссылку';
+$txt['um_menu_same_window'] = 'В том же окне';
+$txt['um_menu_new_tab'] = 'В новой вкладке';
+
+// Submission errors
+$txt['um_menu_not_found'] = 'Такой пункт меню не существует!';
+$txt['um_menu_errors_create'] = 'Возникла ошибка при добавлении пункта меню:';
+$txt['um_menu_errors_modify'] = 'Возникла ошибка при редактировании пункта меню:';
+$txt['um_menu_numeric_desc'] = 'Название пункта меню состоит из одних цифр. Должна быть, хотя бы одна буква или символ.';
+$txt['um_menu_empty_name'] = 'Не указано название пункта меню.';
+$txt['um_menu_empty_link'] = 'Не указана ссылка.';
+$txt['um_menu_empty_parent'] = 'Не выбран родительский раздел.';
+$txt['um_menu_depend_desc'] = 'Вы должны удалить или переместить вложенные пункты, перед тем как редактировать этот пункт менюю.';
+$txt['um_menu_before_child_title'] = 'Сделать пункт меню вложенным';
+$txt['um_menu_before_child_desc'] = 'Используйте "Подраздел", чтобы вложить один пункт меню в другой!';
+$txt['um_menu_mysql'] = 'Такое название пункта меню уже используется!';
