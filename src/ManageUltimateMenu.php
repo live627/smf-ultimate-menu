899c8fb66a8cb8d5279e3d3062a0966f149c2e9d
John Rayes <live627@gmail.com>
2020-11-14 04:59:42 -0700

    parse error

--- src/ManageUltimateMenu.php
+++ src/ManageUltimateMenu.php
@@ -406,7 +406,7 @@ class ManageUltimateMenu
 				'type' => 'forum',
 				'position' => 'before',
 				'status' => 'active',
-				'permissions' => $this->um->listGroups([-3']),
+				'permissions' => $this->um->listGroups([-3]),
 				'parent' => 'home',
 				'id' => 0,
 			);
