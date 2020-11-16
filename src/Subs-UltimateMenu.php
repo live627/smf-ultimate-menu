5d11ba44b87afd9c6ff4966e92e4b230a19b8c31
John Rayes <live627@gmail.com>
2020-11-15 19:27:07 -0700

    refactor Change integration check to use a aubstring

--- src/Subs-UltimateMenu.php
+++ src/Subs-UltimateMenu.php
@@ -15,10 +15,8 @@ function um_load_menu(&$menu_buttons)
 {
 	global $smcFunc, $user_info, $scripturl, $context, $modSettings;
 
-	// Make damn sure we ALWAYS losd last. Priority: 100!
-	$hooks = explode(',', $modSettings['integrate_menu_buttons']);
-	$hook = end($hooks);
-	if (strpos($hook, 'um_load_menu') === false)
+	// Make damn sure we ALWAYS load last. Priority: 100!
+	if (substr($modSettings['integrate_menu_buttons'], -12) !== 'um_load_menu')
 	{
 		remove_integration_function('integrate_menu_buttons', 'um_load_menu');
 		add_integration_function('integrate_menu_buttons', 'um_load_menu');
