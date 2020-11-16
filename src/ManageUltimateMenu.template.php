a8bbfe2124213aa61cdabde6c56177a126ab0a5a
John Rayes <live627@gmail.com>
2020-11-15 19:47:31 -0700

    moar whitespace

--- src/ManageUltimateMenu.template.php
+++ src/ManageUltimateMenu.template.php
@@ -82,7 +82,7 @@ function template_main()
 								<option value="%s"%s>%s</option>',
 			$idx,
 			$context['button_data']['parent'] ==  $idx ? ' selected="selected"' : '',
-			empty($title[0]) ? $title[1] : str_repeat('&emsp;', $title[0]) . $title[1]
+			str_repeat('&emsp;', $title[0]*2) . $title[1]
 		);
 
 	echo '
