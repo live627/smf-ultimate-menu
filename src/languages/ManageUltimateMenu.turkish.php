da8868a2dfb69a50e66209baec4bfdf3539e3ab7
Chen Zhen <github.underdog@gmail.com>
2026-04-05 21:41:09 -0400

    Additional changes
    - fixed Turkish language file
    - every file terminated with a LF

--- src/languages/ManageUltimateMenu.turkish.php
+++ src/languages/ManageUltimateMenu.turkish.php
@@ -1,8 +1,5 @@
 <?php
-
 declare(strict_types=1);
-
-// tïż½rkïż½e ïż½eviri http://smf.konusal.com hizmetidir
 
 /**
  * @package Ultimate Menu mod
@@ -12,56 +9,55 @@ declare(strict_types=1);
  * @license http://opensource.org/licenses/MIT MIT
  */
 
-$txt['um_admin_menu'] = 'Geliïż½miïż½ Menïż½';
-$txt['um_admin_add_button'] = 'Buton Ekle';
-$txt['um_admin_manage_menu'] = 'Menïż½ Dïż½zenle';
+$txt['um_admin_menu'] = 'En Kapsamlı Menü';
+$txt['um_admin_add_button'] = 'Düğme Ekle';
+$txt['um_admin_manage_menu'] = 'Menüyü Yönet';
 $txt['um_admin_menu_opt_file'] = 'Simge Seç';
-
-$txt['admin_menu'] = 'Menïż½';
-$txt['admin_menu_title'] = 'Menïż½ Ayarlarïż½';
-$txt['admin_menu_desc'] = 'Bu sayfa ïż½zel menïż½ butonlarïż½ eklemeyi ve dïż½zenlemeyi saïż½lar.';
-
-$txt['admin_manage_menu_submit'] = 'Gïż½nder';
-$txt['admin_manage_menu_desc'] = 'Oluïż½turduïż½unuz Menïż½ Butonlarïż½nïż½ yïż½netin';
-$txt['admin_menu_add_button_desc'] = 'Menïż½ye Yeni Butonlar Ekleyin';
-
-$txt['um_menu_external_link'] = 'Forum Dïż½ïż½ïż½ Link';
-$txt['um_menu_forum_link'] = 'Forum iïż½i Link';
+$txt['um_admin_manage_icons'] = 'Simgeleri Yönet';
+$txt['admin_menu'] = 'Menü';
+$txt['admin_menu_title'] = 'Menü Ayarları';
+$txt['admin_menu_desc'] = 'Bu sayfa, özel menü düğmeleri eklemenize ve düzenlemenize olanak tanır.';
+$txt['admin_manage_menu_submit'] = 'Gönder';
+$txt['admin_manage_menu_desc'] = 'Oluşturduğunuz Menü düğmelerini yönetin';
+$txt['admin_menu_add_button_desc'] = 'Menüye yeni düğmeler ekleyin';
+$txt['admin_manage_icons_desc'] = 'Yüklediğiniz simgeleri yönetin';
+$txt['um_menu_external_link'] = 'Harici Bağlantı';
+$txt['um_menu_forum_link'] = 'Forum Bağlantısı';
 $txt['um_menu_active'] = 'Aktif';
-$txt['um_menu_inactive'] = 'Pasif';
-$txt['um_menu_no_buttons'] = 'Henïż½z Buton Bulunmamaktadïż½r...';
-$txt['um_menu_button_id'] = 'Buton ID';
-$txt['um_menu_button_upload'] = 'Resim Yükleme';
-$txt['um_menu_button_name'] = 'Buton ïż½smi';
-$txt['um_menu_button_type'] = 'Buton Tïż½rïż½';
-$txt['um_menu_button_position'] = 'Buton pozisyonu';
-$txt['um_menu_button_link'] = 'Buton Link';
-$txt['um_menu_actions'] = 'ïż½ïż½lemler';
-$txt['um_menu_modify'] = 'Dïż½zenle';
-$txt['um_menu_before'] = 'ïż½nce';
-$txt['unchanged'] = 'deïż½iïż½tirilmemiïż½';
-$txt['um_menu_remove_selected'] = 'Seïż½ilmiïż½ Butonlarïż½ kaldïż½r';
-$txt['um_menu_remove_all'] = 'Tïż½m Butonlarïż½ kaldïż½r';
-$txt['um_menu_remove_confirm'] = 'Seïż½ilen Butonlarïż½ kaldïż½rmak istediïż½inizden emin misiniz?';
-$txt['um_menu_remove_all_confirm'] = 'Bïż½tïż½n Butonlarïż½ kaldïż½rmak istediïż½inizden emin misiniz?';
-$txt['um_menu_add_title'] = 'Buton Ekle';
-$txt['um_menu_edit_title'] = 'Buton Dïż½zenle';
-$txt['um_menu_button_name'] = 'Buton ïż½smi';
-$txt['um_menu_button_type'] = 'Buton Tïż½rïż½';
-$txt['um_menu_external'] = 'Forum Dïż½ïż½ïż½ Link';
-$txt['um_menu_forum'] = 'Forum iïż½i Link';
-$txt['um_menu_button_link_desc'] = 'Forum iïż½i Linklerde "index.php?" sonraki kïż½sïż½mïż½ koyabilirsiniz';
-$txt['um_menu_button_link'] = 'Buton Link';
-$txt['um_menu_button_perms'] = 'ïż½zin Gruplar';
-$txt['um_menu_button_guest'] = 'Ziyaretïż½iler';
-$txt['um_menu_button_position'] = 'Buton pozisyonu';
-$txt['um_menu_button_status'] = 'Buton Durumu';
+$txt['um_menu_inactive'] = 'Aktif Değil';
+$txt['um_menu_no_buttons'] = 'Henüz düğme yok...';
+$txt['um_menu_button_id'] = 'Düğme Kimliği';
+$txt['um_menu_button_name'] = 'Düğme Adı';
+$txt['um_menu_button_upload'] = 'Resim Yükle';
+$txt['um_menu_button_type'] = 'Düğme Türü';
+$txt['um_menu_button_position'] = 'Düğme Konumu';
+$txt['um_menu_button_link'] = 'Düğme Bağlantısı';
+$txt['um_menu_actions'] = 'İşlemler';
+$txt['um_menu_modify'] = 'Değiştir';
+$txt['um_menu_before'] = 'Önce';
+$txt['um_menu_child_of'] = 'Çocuk';
+$txt['um_menu_after'] = 'Sonra';
+$txt['um_menu_remove_selected'] = 'Seçili Düğmeleri Kaldır';
+$txt['um_menu_remove_all'] = 'Tüm Düğmeleri Kaldır';
+$txt['um_menu_remove_confirm'] = 'Seçili düğmeleri kaldırmak istediğinizden emin misiniz?';
+$txt['um_menu_remove_all_confirm'] = 'Tüm düğmeleri kaldırmak istediğinizden emin misiniz?';
+$txt['um_menu_add_title'] = 'Düğme Ekle';
+$txt['um_menu_edit_title'] = 'Düzenleme Düğmesi';
+$txt['um_menu_button_name'] = 'Düğme Adı';
+$txt['um_menu_button_type'] = 'Düğme Türü';
+$txt['um_menu_external'] = 'Harici Bağlantı';
+$txt['um_menu_forum'] = 'Forum Bağlantısı';
+$txt['um_menu_button_link_desc'] = 'Forum bağlantısı için, forumunuzun bağlantısında "index.php?" kısmından sonraki kısmı ekleyebilirsiniz.';
+$txt['um_menu_button_link'] = 'Düğme Bağlantısı';
+$txt['um_menu_button_perms'] = 'İzin Verilen Gruplar';
+$txt['um_menu_button_guest'] = 'Misafirler';
+$txt['um_menu_button_position'] = 'Düğme Konumu';
+$txt['um_menu_button_status'] = 'Düğme Durumu';
 $txt['um_menu_button_active'] = 'Aktif';
-$txt['um_menu_button_inactive'] = 'Pasif';
-$txt['um_menu_link_type'] = 'Link Tïż½rïż½';
-$txt['um_menu_same_window'] = 'Aynïż½ Pencere';
+$txt['um_menu_button_inactive'] = 'Aktif Değil';
+$txt['um_menu_link_type'] = 'Bağlantı Türü';
+$txt['um_menu_same_window'] = 'Aynı Pencere';
 $txt['um_menu_new_tab'] = 'Yeni Sekme';
-
 $txt['um_menu_button_icon'] = 'Düğme Simgesi';
 $txt['um_menu_no_icons'] = 'Hiçbir Ultimate Menü simgesi yok';
 $txt['um_menu_icon_name'] = 'Dosya Adı';
@@ -76,18 +72,19 @@ $txt['um_menu_icon_unassigned'] = '&#10062;';
 $txt['um_menu_icons_uninstall'] = 'Tüm Ultimate Menu simgelerini "um_icons" yolundan kaldırın';
 
 // Gönderim hataları
-$txt['um_menu_session_verify_fail'] = 'Session verification failed. Please then try again.';
-$txt['um_menu_not_found'] = 'Mevcut Olmayan Bir Butonu Dïż½zenlemeye ïż½alïż½ïż½tïż½nïż½z!';
-$txt['um_menu_errors_create'] = 'Butonunuz eklenirken ïż½u hatalar oluïż½tu:';
-$txt['um_menu_errors_modify'] = 'Butonunuz dïż½zenlenirken ïż½u hatalar oluïż½tu:';
-$txt['um_menu_numeric_desc'] = 'Seïż½tiïż½iniz buton adïż½ tïż½mïż½ sayïż½sal ise. En azïż½ndan olmayan bir sayïż½sal karakter iïż½eren bir ad kullanmanïż½z gerekir..<br />
-1E5 sayïż½sal (bilimsel gïż½sterim) olarak kabul edilir. 1.5 Sayïż½sal (ondalïż½k sayïż½) olarak kabul edilir';
-$txt['um_menu_empty_name'] = 'Buton ïż½smi alanïż½ boïż½ bïż½rakïż½lmïż½ïż½tïż½r.';
-$txt['um_menu_empty_link'] = 'Buton Link boïż½ bïż½rakïż½lmïż½ïż½tïż½r.';
-$txt['um_menu_empty_parent'] = 'Ebeveyni boïż½ bïż½rakïż½lmïż½ïż½tïż½r.';
-$txt['um_menu_depend_desc'] = 'Taïż½ïż½madan veya deïż½iïż½tirmeden ïż½nce bu butonun tïż½m alt butonlarïż½nïż½ kaldïż½rmalïż½sïż½nïż½z.';
-$txt['um_menu_before_child_title'] = 'Bunu alt buton yapïż½n';
-$txt['um_menu_before_child_desc'] = 'butonlarïż½ baïż½ka alt buton yapmak iïż½in alt butonu kullanïż½n!';
-$txt['um_menu_mysql'] = 'Seïż½tiïż½iniz buton adïż½ zaten kullanïż½lïż½yor!';
+$txt['um_menu_session_verify_fail'] = 'Oturum doğrulama başarısız oldu. Lütfen tekrar deneyin.';
+$txt['um_menu_not_found'] = 'Düzenlemeye çalıştığınız düğme mevcut değil!';
+$txt['um_menu_errors_create'] = 'Düğmenizi eklerken aşağıdaki hata veya hatalar oluştu:';
+$txt['um_menu_errors_modify'] = 'Düğmenizi düzenlerken aşağıdaki hata veya hatalar oluştu:';
+$txt['um_menu_numeric_desc'] = 'Seçtiğiniz düğme adı tamamen sayısal. En az bir sayısal olmayan karakter içeren bir ad kullanmalısınız.<br />1e5 sayısal (bilimsel gösterim) olarak kabul edilir, 1.5 sayısal (ondalık sayı) olarak kabul edilir';
+$txt['um_menu_empty_name'] = 'Ad boş bırakıldı.';
+$txt['um_menu_empty_link'] = 'Bağlantı boş bırakıldı.';
+$txt['um_menu_empty_parent'] = 'Üst öğe boş bırakıldı.';
+$txt['um_menu_depend_desc'] = 'Bu düğmeyi değiştirmeden önce, bu düğmenin tüm alt ve/veya torun düğmelerini taşımanız veya kaldırmanız gerekir.';
+$txt['um_menu_before_child_title'] = 'Bunu bir alt düğme yap';
+$txt['um_menu_before_child_desc'] = 'Düğmeleri başka bir düğmenin alt öğesi yapmak için "Child Of" özelliğini kullanın!';
+$txt['um_menu_mysql'] = 'Seçtiğiniz düğme adı zaten kullanımda!';
 $txt['um_menu_filename_illegal'] = 'Girilen dosya adı geçersiz karakterler içeriyor.';
-$txt['um_menu_filename_exists'] = 'Girilen dosya adı, Ultimate Menu simgeleri yolunda mevcut değil.';
+$txt['um_menu_filename_exists'] = 'Girilen dosya adı Ultimate Menu simgeleri yolunda mevcut değil.';
+$txt['um_menu_filename_compress'] = 'Dosya kullanılabilir ancak sıkıştırılamaz.';
+$txt['um_menu_filename_unknown'] = 'Resim dosyasında bilinmeyen bir hata oluştu.';
