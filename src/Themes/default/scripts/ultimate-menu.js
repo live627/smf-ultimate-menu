d59af9628c00aa4ed572aaff481c32b381b98db8
Chen Zhen <github.underdog@gmail.com>
2026-04-15 21:43:54 -0400

    Added custom select and no script fallback
    - custom select menu (styled ul/list)
    - no script fallback for regular select
    - no script fallback for file upload
    - fixed console logging for file name

--- src/Themes/default/scripts/ultimate-menu.js
+++ src/Themes/default/scripts/ultimate-menu.js
@@ -31,13 +31,27 @@ if (umdiv) {
 	});
 }
 $(document).ready(function() {
+	var $umInput = $("<input>", {
+		id: "um_icon",
+		type: "hidden",
+		name: "icon",
+		value: $('#um_icon').find(":selected").val(),
+	}), $umJqInput = $("<input>", {
+		id: "um_jq",
+		type: "hidden",
+		name: "um_jq",
+		value: "1",
+	}), $um_ul = $("<ul />", {
+		class: "um_icons windowbg",
+		id: "um_list"
+	}), $um_nofile = $("select#um_icon_select option:first").text(),
+		$um_selected = $("select#um_icon_select option:selected").text();
+	$("#advum_icons").css("display","flex").addClass("advum_icons");
 	$("#um_file").on("change", function(e) {
 		if (!um_secureCode) {
 			return false;
 		}
-		var fileData = $("#um_file").prop("files")[0];
-		var fileName = e.target.files[0].name;
-		var formData = new FormData();
+		var fileData = $("#um_file").prop("files")[0], fileName = e.target.files[0].name, formData = new FormData();
 		formData.append("attachment", fileData);
 		formData.append(smf_session_var, smf_session_id);
 		formData.append("um_checkcode", um_secureCode);
@@ -51,15 +65,17 @@ $(document).ready(function() {
 			data: formData,
 			type: "POST",
 			success: function(response) {
-				console.log("Upload successful: " + response);
+				console.log("Upload successful: " + JSON.stringify(response.file, null, 2));
 				$("#um_file").val("");
 				if (response.file) {
-					$("#um_icon_img").attr("src", smf_default_theme_url + "/images/um_icons/" + response.file);
-					$("#um_icon").append($("<option>", {
-						value: response.file,
-						text: response.file,
-						selected: true
-					}));
+					$("#um_icon_img").attr("src", smf_default_theme_url + "/images/um_icons/" + $.trim(response.file));
+					$(".ultimateMenu_drop>ul li").removeClass("um_icon_selected").addClass("um_icon");
+					$("<li />", {
+						"data-value": $.trim(response.file),
+						text: $.trim(response.file),
+						class: "um_icon_selected"
+					}).appendTo($um_ul);
+					$(".hideSelect").text(response.file);
 				}
 				else if (response.error) {
 					console.log(response.error);
@@ -77,7 +93,6 @@ $(document).ready(function() {
 	$("#um_icon").on("change", function() {
 		if ($(this).val()) {
 			$("#um_icon_img").attr("src", smf_default_theme_url + "/images/um_icons/" + $(this).val());
-			$(this).prop("selected", true);
 		}
 	});
 	$("img#um_icon_img").on("mouseover", function() {
@@ -88,7 +103,7 @@ $(document).ready(function() {
 			top: "-25px",
 			left: "-25px"
 		}, "medium");
-	}).on( "mouseout", function() {
+	}).on("mouseout", function() {
 		$(this).css("cursor", "default");
 		$(this).stop().animate({
 			width: "16px",
@@ -97,4 +112,29 @@ $(document).ready(function() {
 			left: "0px"
 		}, "medium");
 	});
+	$("#um_icon_select").find("option").each(function() {
+		$("<li />", {
+			"data-value": $.trim($(this).val()),
+			text: $.trim($(this).text()),
+		}).appendTo($um_ul);
+	});
+	$("#um_icon_select").remove();
+	$um_ul.appendTo(".ultimateMenu_drop");
+	$(".hideSelect").css("display", "inline-block");
+	$(".hideSelect").text( (!$("input#um_icon").val() ? $.trim($um_selected) : $.trim($("input#um_icon").val())));
+	$umInput.appendTo('#um_icon_list');
+	$(".ultimateMenu_drop").on("click", function(e) {
+      e.stopPropagation();
+      $(".ultimateMenu_drop>ul").stop().slideToggle(1000);
+      $(document).on("click", function() {
+        $(".ultimateMenu_drop>ul").hide();
+      });
+    });
+    $(".ultimateMenu_drop>ul li").on('click', function() {
+		$(".ultimateMenu_drop>ul li").removeClass("um_icon_selected").addClass("um_icon");
+		$(this).removeClass("um_icon").addClass("um_icon_selected");
+		$(".hideSelect").text($.trim($(this).text()));
+		$("input#um_icon").val($.trim($(this).data("value")));
+		$("img#um_icon_img").attr("src", smf_default_theme_url + "/images/um_icons/" + $.trim($(this).data("value")));
+    });
 });
