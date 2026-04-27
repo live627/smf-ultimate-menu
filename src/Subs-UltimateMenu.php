07bbf23eb8f594f34b2abe94d9567d54c1fe3ba3
John Rayes <live627@gmail.com>
2026-04-26 17:42:10 -0700

    fix tests; add more assertions

--- src/Subs-UltimateMenu.php
+++ src/Subs-UltimateMenu.php
@@ -12,13 +12,14 @@ declare(strict_types=1);
 
 function um_load_menu(&$menu_buttons): void
 {
-	global $smcFunc, $user_info, $scripturl, $modSettings;
+	global $context, $modSettings, $smcFunc, $user_info, $scripturl;
 
 	// Make damn sure we ALWAYS load last. Priority: 100!
-	if (substr($modSettings['integrate_menu_buttons'], -12) !== 'um_load_menu')
+	if (!isset($context['um_replaying_menu']) && substr($modSettings['integrate_menu_buttons'], -12) !== 'um_load_menu')
 	{
 		remove_integration_function('integrate_menu_buttons', 'um_load_menu');
 		add_integration_function('integrate_menu_buttons', 'um_load_menu');
+
 		return;
 	}
 
