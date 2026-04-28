63063e607eacd8382cf719a4238dd923f91bf46a
Chen Zhen <github.underdog@gmail.com>
2026-04-28 03:14:08 -0400

    Step 3 - Language Files
    - added language files

---
+++ src/languages/ManageUltimateMenu.turkish.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package Ultimate Menu mod
+ * @version   2.0.4
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2026, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+$txt['um_admin_menu_um'] = 'En Kapsamlı Menü';
+$txt['um_admin_add_button'] = 'Düğme Ekle';
+$txt['um_admin_manage_menu'] = 'Menüyü Yönet';
+$txt['um_admin_menu_um_opt_file'] = 'Simge Seç';
+$txt['um_admin_manage_icons'] = 'Simgeleri Yönet';
+
+$txt['admin_menu_um'] = 'Menü';
+$txt['admin_menu_um_title'] = 'Menü Ayarları';
+$txt['admin_menu_um_desc'] = 'Bu sayfa, özel menü düğmeleri eklemenize ve düzenlemenize olanak tanır.';
+
+$txt['admin_manage_um_submit'] = 'Gönder';
+$txt['admin_manage_um_desc'] = 'Oluşturduğunuz Menü düğmelerini yönetin';
+$txt['admin_menu_um_add_button_desc'] = 'Menüye yeni düğmeler ekleyin';
+$txt['admin_manage_um_icons_desc'] = 'Yüklediğiniz simgeleri yönetin';
+
+$txt['um_menu_external_link'] = 'Harici Bağlantı';
+$txt['um_menu_forum_link'] = 'Forum Bağlantısı';
+$txt['um_menu_active'] = 'Aktif';
+$txt['um_menu_inactive'] = 'Aktif Değil';
+$txt['um_menu_no_buttons'] = 'Henüz düğme yok...';
+$txt['um_menu_button_id'] = 'Düğme Kimliği';
+$txt['um_menu_button_name'] = 'Düğme Adı';
+$txt['um_menu_button_upload'] = 'Resim Yükle';
+$txt['um_menu_button_type'] = 'Düğme Türü';
+$txt['um_menu_button_position'] = 'Düğme Konumu';
+$txt['um_menu_button_link'] = 'Düğme Bağlantısı';
+$txt['um_menu_actions'] = 'İşlemler';
+$txt['um_menu_modify'] = 'Değiştir';
+$txt['um_menu_before'] = 'Önce';
+$txt['um_menu_child_of'] = 'Soyundan Gelen';
+$txt['um_menu_after'] = 'Sonra';
+$txt['um_menu_remove_selected'] = 'Seçili Düğmeleri Kaldır';
+$txt['um_menu_remove_all'] = 'Tüm Düğmeleri Kaldır';
+$txt['um_menu_remove_confirm'] = 'Seçili düğmeleri kaldırmak istediğinizden emin misiniz?';
+$txt['um_menu_remove_all_confirm'] = 'Tüm düğmeleri kaldırmak istediğinizden emin misiniz?';
+$txt['um_menu_add_title'] = 'Düğme Ekle';
+$txt['um_menu_edit_title'] = 'Düzenleme Düğmesi';
+$txt['um_menu_button_name'] = 'Düğme Adı';
+$txt['um_menu_button_type'] = 'Düğme Türü';
+$txt['um_menu_external'] = 'Harici Bağlantı';
+$txt['um_menu_forum'] = 'Forum Bağlantısı';
+$txt['um_menu_button_link_desc'] = 'Forum bağlantısı için, forumunuzun bağlantısında "index.php?" kısmından sonraki kısmı ekleyebilirsiniz.';
+$txt['um_menu_button_link'] = 'Düğme Bağlantısı';
+$txt['um_menu_button_perms'] = 'İzin Verilen Gruplar';
+$txt['um_menu_button_guest'] = 'Misafirler';
+$txt['um_menu_button_position'] = 'Düğme Konumu';
+$txt['um_menu_button_status'] = 'Düğme Durumu';
+$txt['um_menu_button_active'] = 'Aktif';
+$txt['um_menu_button_inactive'] = 'Aktif Değil';
+$txt['um_menu_link_type'] = 'Bağlantı Türü';
+$txt['um_menu_same_window'] = 'Aynı Pencere';
+$txt['um_menu_new_tab'] = 'Yeni Sekme';
+
+$txt['um_menu_button_icon'] = 'Düğme Simgesi';
+$txt['um_menu_no_icons'] = 'Hiçbir Ultimate Menü simgesi yok';
+$txt['um_menu_icon_name'] = 'Dosya Adı';
+$txt['um_menu_icon_assigned'] = 'Atanan Düğme';
+$txt['um_menu_delete_selected'] = 'Seçili Simge Dosyalarını Sil';
+$txt['um_menu_delete_unassigned'] = 'Atanmamış Simge Dosyalarını Sil';
+$txt['um_menu_delete_all'] = 'Tüm Simge Dosyalarını Sil';
+$txt['um_menu_standardize_all'] = 'Dosyaları Standartlaştır';
+$txt['um_menu_standardize_all_confirm'] = 'Tüm dosya adlarını ve görüntü boyutlarını standartlaştırmak istediğinizden emin misiniz?';
+$txt['um_menu_delete_selected_confirm'] = 'Seçili simge dosyalarını silmek istediğinizden emin misiniz?';
+$txt['um_menu_delete_unassigned_confirm'] = 'Atanmamış tüm simge dosyalarını silmek istediğinizden emin misiniz?';
+$txt['um_menu_delete_all_confirm'] = 'Tüm simge dosyalarını silmek istediğinizden emin misiniz?';
+$txt['um_menu_icons_uninstall'] = 'Tüm Ultimate Menu simgelerini "um_icons" yolundan kaldırın';
+$txt['um_menu_icons_none'] = 'Hiç';
+$txt['um_menu_icon_unassigned'] = '&#11160; Atanmamış';
+$txt['um_menu_icon_assigned_button'] = '&#8658; %s';
+$txt['um_menu_icon_unstandardized'] = '&#11079; Standartlaştırılmamış';
+
+$txt['um_menu_button_sprite_generate'] = 'Oluştur';
+$txt['um_menu_button_sprite_generate_confirm'] = 'Bu, mevcut tüm Ultimate Menü düğme simgelerini içeren yeni bir sprite oluşturacaktır.\n\nOnay kutusu seçilirse, simge seti olan her düğmeyi sprite kullanacak şekilde otomatik olarak ayarlayacaktır.\n\nBunu yapmak istediğinizden emin misiniz?';
+$txt['um_menu_button_sprite_info'] = 'Sprite\'lar HTTP isteklerini azaltarak sayfa yüklemelerini hızlandırır.';
+$txt['um_menu_button_sprite_detected'] = 'Sprite Mevcut';
+$txt['um_menu_button_sprite_undetected'] = 'Sprite Kullanılamaz';
+$txt['um_menu_sprite_active'] = 'Sprite';
+$txt['um_menu_sprite_inactive'] = 'İkon';
+$txt['um_menu_button_sprite_generated'] = 'Sprite başarıyla oluşturuldu!';
+
+// Gönderim hataları
+$txt['um_menu_session_verify_fail'] = 'Oturum doğrulama başarısız oldu. Lütfen tekrar deneyin.';
+$txt['um_menu_not_found'] = 'Düzenlemeye çalıştığınız düğme mevcut değil!';
+$txt['um_menu_errors_create'] = 'Düğmenizi eklerken aşağıdaki hata veya hatalar oluştu:';
+$txt['um_menu_errors_modify'] = 'Düğmenizi düzenlerken aşağıdaki hata veya hatalar oluştu:';
+$txt['um_menu_numeric_desc'] = 'Seçtiğiniz düğme adı tamamen sayısal. En az bir sayısal olmayan karakter içeren bir ad kullanmalısınız.<br />1e5 sayısal (bilimsel gösterim) olarak kabul edilir, 1.5 sayısal (ondalık sayı) olarak kabul edilir';
+$txt['um_menu_empty_name'] = 'Ad boş bırakıldı.';
+$txt['um_menu_empty_link'] = 'Bağlantı boş bırakıldı.';
+$txt['um_menu_empty_parent'] = 'Üst öğe boş bırakıldı.';
+$txt['um_menu_depend_desc'] = 'Bu düğmeyi değiştirmeden önce, bu düğmenin tüm alt ve/veya torun düğmelerini taşımanız veya kaldırmanız gerekir.';
+$txt['um_menu_before_child_title'] = 'Bunu bir alt düğme yap';
+$txt['um_menu_before_child_desc'] = 'Düğmeleri başka bir düğmenin alt öğesi yapmak için "Child Of" özelliğini kullanın!';
+$txt['um_menu_mysql'] = 'Seçtiğiniz düğme adı zaten kullanımda!';
+$txt['um_menu_filename_illegal'] = 'Girilen dosya adı geçersiz karakterler içeriyor.';
+$txt['um_menu_filename_exists'] = 'Girilen dosya adı Ultimate Menu simgeleri yolunda mevcut değil.';
+$txt['um_menu_filename_compress'] = 'Dosya kullanılabilir ancak sıkıştırılamaz.';
+$txt['um_menu_filename_unknown'] = 'Resim dosyasında bilinmeyen bir hata oluştu.';
+
+$txt['um_menu_sprite_create'] = 'Boş sprite görüntüsü oluşturulamadı.';
+$txt['um_menu_sprite_sourcepath'] = 'Kaynak yolu mevcut değil: %s';
+$txt['um_menu_sprite_savepath'] = 'Kaydetme yolu mevcut değil: %s';
+$txt['um_menu_sprite_csspath'] = 'CSS yolu mevcut değil: %s';
+$txt['um_menu_button_sprite_error'] = 'Sprite oluşturma girişiminde bir hata oluştu.\nLütfen dosya izinlerini kontrol edin ve tekrar deneyin.';
+$txt['um_menu_button_sprite_drivel'] = 'Amaçlanan mesaj mantıksız!';
