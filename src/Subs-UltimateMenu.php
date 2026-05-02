f5864865422a5bdd51f5a62e78ec6c8a4a717d68
Chen Zhen <github.underdog@gmail.com>
2026-05-02 00:27:16 -0400

    Update Subs-UltimateMenu.php

--- src/Subs-UltimateMenu.php
+++ src/Subs-UltimateMenu.php
@@ -38,7 +38,7 @@ function um_load_menu(&$menu_buttons): void
 		];
 
 		recursive_button($temp_menu, $menu_buttons, $row['parent'], $row['position'], $key);
-		$context['um_all_buttons'] = $menu_buttons;
+		$context['um_all_buttons'] = $context['um_all_buttons'] ?? $menu_buttons;
 	}
 }
 
